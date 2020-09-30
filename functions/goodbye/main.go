f7b422dd0ce11486ead78991dd34c7cd4f2dd99d
Ship <mnshiplet@gmail.com>
2020-09-30 13:00:34 -0600

    update file tree

---
+++ functions/goodbye/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+"github.com/aws/aws-lambda-go/events"
+"github.com/aws/aws-lambda-go/lambda"
+"net/http"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{"Content-Type": "text/plain", "Access-Control-Allow-Origin": "michaelshiplet.com"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body: "Goodbye friend!",
+		IsBase64Encoded: false,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+
